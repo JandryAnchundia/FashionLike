a15d72f88965e8f6d127c47192373d43c1c717cf
franciscobarrera695 <franciscobarrera968@gmail.com>
2022-01-29 01:36:59 -0300

    por el momento para mandar mails una vez que te registras queda en suspenso hasta encontrar una alternativa mejor :(

---
+++ src/config/mailer.js
@@ -0,0 +1,23 @@
+
+ /* mandar un email una vez registrado   
+
+     DE MOMENTO NO SE VA A USAR HASTA ENCONTRAR UNA ALTERNATIVA MEJOR
+    const mailgun = require("mailgun-js");
+
+
+    const API_KEY = '2a9b9faa29f3b2980deddb4e7c8b8880-054ba6b6-5905d850';
+    const DOMAIN = 'sandbox06638c2430c6407db2d7e8b4759d1b91.mailgun.org';
+    
+    const mg = mailgun({apiKey: API_KEY, domain: DOMAIN});
+
+    const data = {
+	from: 'Excited User <franciscobarrera968@gmail.com>',
+	to: 'franciscobarrera968@gmail.com',
+	subject: 'Felicidades!',
+	text: 'Te registraste correctamente'
+    };
+    await mg.messages().send(data)
+    */
+
+
+    
