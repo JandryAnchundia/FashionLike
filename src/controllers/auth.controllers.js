5b21b6eaca938a90324f7837840024adf47910cc
olartejohn <olartejohn@gmail.com>
2022-01-31 17:53:13 -0500

    login primer avance

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -17,5 +17,43 @@ export const registro = async(req,res) => {
         message:'user create success',
         user
     })
+
+    
+    
+}
+
+export const login = function(passport) {
+    passport.use(
+        new LocalStrategy({usernameField : 'email'},(email,password,done)=> {
+                //match user
+                User.findOne({email : email})
+                .then((user)=>{
+                 if(!user) {
+                     return done(null,false,{message : 'that email is not registered'});
+                 }
+                 //match pass
+                 bcrypt.compare(password,user.password,(err,isMatch)=>{
+                     if(err) throw err;
+
+                     if(isMatch) {
+                         return done(null,user);
+                     } else {
+                         return done(null,false,{message : 'pass incorrect'});
+                     }
+                 })
+                })
+                .catch((err)=> {console.log(err)})
+        })
+        
+    )
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      });
+      
+      passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+          done(err, user);
+        });
+      }); 
 }
 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,5 +6,7 @@ import * as authControllers from '../controllers/auth.controllers';
 const router = Router()
 
 router.post('/register',authControllers.registro)
+router.post('/login',authControllers.login)
+
 
 export default router
